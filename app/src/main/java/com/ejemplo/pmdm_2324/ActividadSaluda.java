f2d97a09087c1b72ed809ba6aa0310a50bb128d0
Álvaro <koksclashs@gmail.com>
2023-09-26 17:48:13 +0200

    Mi primera práctica

---
+++ app/src/main/java/com/ejemplo/pmdm_2324/ActividadSaluda.java
@@ -0,0 +1,14 @@
+package com.ejemplo.pmdm_2324;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadSaluda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ut01_actividad_saluda);
+    }
+}
