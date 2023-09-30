a0b7d7ae457542ae02bcb5a1ee8eb8cdc4be7016
Álvaro <koksclashs@gmail.com>
2023-09-30 11:11:14 +0200

    AppColoresTerminada

--- app/src/main/java/com/ejemplo/pmdm_2324/ut02/u2a2Coloneitor.java
+++ app/src/main/java/com/ejemplo/pmdm_2324/ut02/u2a2Coloneitor.java
@@ -2,10 +2,13 @@ package com.ejemplo.pmdm_2324.ut02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.ejemplo.pmdm_2324.R;
@@ -17,17 +20,42 @@ public class u2a2Coloneitor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u2a2_coloneitor);
 
-        SeekBar sbColor;
+        SeekBar sbRojo;
         Button btnGenerar;
-        TextView dimeColor;
+        TextView muestraColor;
+        Switch cambiarColor;
+        EditText dimeColor;
+        SeekBar sbVerde;
+        SeekBar sbAzul;
 
-        sbColor = findViewById(R.id.u2a2idsbRojo);
+        sbRojo = findViewById(R.id.u2a2idsbRojo);
         btnGenerar = findViewById(R.id.u2a2idbtGenerar);
-        dimeColor = findViewById(R.id.u2a2idtvDimeColor);
+        muestraColor = findViewById(R.id.u2a2idtvDimeColor);
+        dimeColor = findViewById(R.id.u2a2idetColor);
+        cambiarColor = findViewById((R.id.u2a1idswContraste));
+        sbVerde = findViewById(R.id.u2a2idsbVerde);
+        sbAzul = findViewById(R.id.u2a2idsbAzul);
 
         btnGenerar.setOnClickListener((View v) -> {
-            int potenciaColor = sbColor.getProgress();
-            dimeColor.setText(String.valueOf(potenciaColor));
+
+            if(dimeColor.getText().toString().equalsIgnoreCase("Rojo") || dimeColor.getText().toString().equalsIgnoreCase("Verde")
+            || dimeColor.getText().toString().equalsIgnoreCase("Azul")){
+                muestraColor.setText(dimeColor.getText());
+            }else{
+                muestraColor.setText("Color ingresado incorrecto.");
+            }
+
+            int potenciaColorRojo = sbRojo.getProgress();
+            int potenciaColorVerde = sbVerde.getProgress();
+            int potenciaColorAzul = sbAzul.getProgress();
+
+            int colorElegido = Color.rgb(potenciaColorRojo, potenciaColorVerde, potenciaColorAzul);
+            muestraColor.setBackgroundColor(colorElegido);
+
+            if(cambiarColor.isChecked()){
+                int colorBlanco = Color.WHITE;
+                muestraColor.setBackgroundColor(colorBlanco);
+            }
         });
     }
 }
