8d3c5b060183aa7f4c22ec2da6d38ca041e5648e
Álvaro <koksclashs@gmail.com>
2023-10-05 10:33:33 +0200

    AppCobro terminada

--- app/src/main/java/com/ejemplo/pmdm_2324/ut02/u2a3Propinatron2000.java
+++ app/src/main/java/com/ejemplo/pmdm_2324/ut02/u2a3Propinatron2000.java
@@ -2,9 +2,11 @@ package com.ejemplo.pmdm_2324.ut02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.ejemplo.pmdm_2324.R;
@@ -12,13 +14,15 @@ import com.ejemplo.pmdm_2324.R;
 public class u2a3Propinatron2000 extends AppCompatActivity {
 
     Button btn0,btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnFlecha, btnC;
-    TextView tvmuestraNumeros;
+    RadioButton rbtnMalo, rbtnBueno, rbtnExcelente;
+    TextView tvmuestraNumeros, tvPrecioFin;
     View.OnClickListener controlador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u2a3_propinatron2000);
 
+        //Botones
         btn0 = findViewById(R.id.u2a3btCero);
         btn1 = findViewById(R.id.u2a3btUno);
         btn2 = findViewById(R.id.u2a3btDos);
@@ -31,7 +35,13 @@ public class u2a3Propinatron2000 extends AppCompatActivity {
         btn9 = findViewById(R.id.u2a3btNueve);
         btnFlecha = findViewById(R.id.u2a3btFlecha);
         btnC = findViewById(R.id.u2a3btC);
+        //textView
         tvmuestraNumeros = findViewById(R.id.u2a3tvControlador);
+        tvPrecioFin = findViewById(R.id.u2a3tvPrecioFin);
+        //RadioButtons
+        rbtnMalo = findViewById(R.id.u2a3rdbtnMalo);
+        rbtnBueno = findViewById(R.id.u2a3rdbtnBueno);
+        rbtnExcelente = findViewById(R.id.u2a3rdbtnExcelente);
 
         controlador = (View v) -> {
             Button botonPulsado = (Button) v;
@@ -40,13 +50,24 @@ public class u2a3Propinatron2000 extends AppCompatActivity {
 
             if(v == btnC){
                 tvmuestraNumeros.setText("");
-            }/*else if(v == btnFlecha){
-                tvmuestraNumeros.setText(nuevoTexto.substring(0, nuevoTexto.length() - 1));
-            }*/else{
+            }else if(v == btnFlecha){
+                tvmuestraNumeros.setText(textoActual.substring(0, textoActual.length() - 1));
+            }else if(v != rbtnMalo && v != rbtnBueno && v != rbtnExcelente){
                 nuevoTexto = textoActual + botonPulsado.getText().toString();
                 tvmuestraNumeros.setText(nuevoTexto);
             }
 
+            if(rbtnMalo.isChecked()){
+                tvPrecioFin.setText(textoActual.toString());
+            }else if(rbtnBueno.isChecked()){
+                int precio = Integer.parseInt(textoActual);
+                double resultado = precio * 1.10;
+                tvPrecioFin.setText(String.valueOf(resultado));
+            } else if(rbtnExcelente.isChecked()) {
+                int precio = Integer.parseInt(textoActual);
+                double resultado = precio * 1.20;
+                tvPrecioFin.setText(String.valueOf(resultado));
+            }
         };
 
         btn0.setOnClickListener(controlador);
@@ -59,7 +80,10 @@ public class u2a3Propinatron2000 extends AppCompatActivity {
         btn7.setOnClickListener(controlador);
         btn8.setOnClickListener(controlador);
         btn9.setOnClickListener(controlador);
-        //btnFlecha.setOnClickListener(controlador);
+        btnFlecha.setOnClickListener(controlador);
         btnC.setOnClickListener(controlador);
+        rbtnMalo.setOnClickListener(controlador);
+        rbtnBueno.setOnClickListener(controlador);
+        rbtnExcelente.setOnClickListener(controlador);
     }
 }
