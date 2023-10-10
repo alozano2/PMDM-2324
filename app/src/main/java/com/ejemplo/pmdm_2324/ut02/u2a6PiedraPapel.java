ed1deec7c5e0015c9f794ccea15cda306222bc75
Álvaro <koksclashs@gmail.com>
2023-10-10 17:58:18 +0200

    AppCitaPrevia

---
+++ app/src/main/java/com/ejemplo/pmdm_2324/ut02/u2a6PiedraPapel.java
@@ -0,0 +1,18 @@
+package com.ejemplo.pmdm_2324.ut02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ejemplo.pmdm_2324.R;
+
+public class u2a6PiedraPapel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_u2a6_piedra_papel);
+
+
+    }
+}
