6200c05eaaea8c0951963ece8168ff0be872a5dd
Álvaro <koksclashs@gmail.com>
2023-10-19 10:13:22 +0200

    App de dos Activities(con error en el operador)

--- app/src/main/java/com/ejemplo/pmdm_2324/ut03/u3a2Resultado.java
+++ app/src/main/java/com/ejemplo/pmdm_2324/ut03/u3a2Resultado.java
@@ -3,14 +3,47 @@ package com.ejemplo.pmdm_2324.ut03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.ejemplo.pmdm_2324.R;
 
 public class u3a2Resultado extends AppCompatActivity {
-
+    TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u3a2_resultado);
+
+        Bundle info = getIntent().getExtras();
+
+        String operador = info.getString(u3a2aCalcula.OPERADOR);
+        int num1 = Integer.valueOf(info.getString(u3a2aCalcula.INFO_NUM1));
+        int num2 = Integer.valueOf(info.getString(u3a2aCalcula.INFO_NUM2));
+        double resultado = 0;
+        tvResultado = findViewById(R.id.u3a2tvResultado);
+
+        if(operador.equals("+")){
+            resultado = num1 + num2;
+            tvResultado.setText(String.valueOf(resultado));
+        } else if (operador.equals("-")) {
+            if(num1 > num2){
+                resultado = num2 - num1;
+                tvResultado.setText(String.valueOf(resultado));
+            }else{
+                resultado = num1 - num2;
+                tvResultado.setText(String.valueOf(resultado));
+            }
+        } else if (operador.equals("*")) {
+            resultado = num1 * num2;
+            tvResultado.setText(String.valueOf(resultado));
+        } else if (operador.equals("/")) {
+            if(num2 != 0){
+                resultado = num1 / num2;
+                tvResultado.setText(String.valueOf(resultado));
+            }else{
+                tvResultado.setText("No es posible realizar la division entre cero.");
+            }
+        }
+
     }
 }
--- app/src/main/java/com/ejemplo/pmdm_2324/ut03/u3a2aCalcula.java
+++ app/src/main/java/com/ejemplo/pmdm_2324/ut03/u3a2aCalcula.java
@@ -2,15 +2,46 @@ package com.ejemplo.pmdm_2324.ut03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.ejemplo.pmdm_2324.R;
 
 public class u3a2aCalcula extends AppCompatActivity {
+    public static final String INFO_NUM1 = "CLAVE_NUM";
+    public static final String INFO_NUM2 = "CLAVE_NUM";
+    public static final String OPERADOR = "CLAVE_NUM";
 
+    EditText etNumero1, etNumero2;
+    RadioButton radiobtnSuma, radiobtnResta, radiobtnMultiplica, radiobtnDiv, r;
+    Button btnCalcular;
+    RadioGroup opcionSeleccionada;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u3a2a_calcula);
+
+        etNumero1 = findViewById(R.id.u3a2txtNum1);
+        etNumero2 = findViewById(R.id.u3a2txtNum2);
+        radiobtnSuma = findViewById(R.id.u3a2radiobtnSuma);
+        radiobtnResta = findViewById(R.id.u3a2radiobtnResta);
+        radiobtnMultiplica = findViewById(R.id.u3a2radiobtnMult);
+        radiobtnDiv = findViewById(R.id.u3a2radiobtnDiv);
+        btnCalcular = findViewById(R.id.u3a2btnCalcular);
+        opcionSeleccionada = findViewById(R.id.u3a2radiogrpOp);
+
+        btnCalcular.setOnClickListener(view -> {
+            r = findViewById(opcionSeleccionada.getCheckedRadioButtonId());
+
+            Intent i = new Intent(this, u3a2Resultado.class);
+            i.putExtra(INFO_NUM1, etNumero1.getText().toString());
+            i.putExtra(INFO_NUM2, etNumero2.getText().toString());
+            i.putExtra(OPERADOR, r.getText().toString());
+            startActivity(i);
+        });
     }
 }
